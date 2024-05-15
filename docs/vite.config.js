b3c3539e4d29a7593e427a0e58df61ebc5f9b2fa
Priya140895 <priya@myattest.com>
2024-05-15 21:29:07 +0530

    msgd

--- docs/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://priya140895.github.io/VitePress_Documentation/
-export default defineConfig({
-  plugins: [react()],
-  base: '/VitePress_Documentation/',
-})
